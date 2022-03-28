b00bb80cc58721006b61b5683ffa82a81a664a96
arpho <damicogiuseppe77@gmail.com>
2022-03-28 17:47:25 +0200

    passwordfield
    generated component

---
+++ src/app/modules/dynamic-form/components/password-field/password-field.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { PasswordFieldComponent } from './password-field.component';
+
+describe('PasswordFieldComponent', () => {
+  let component: PasswordFieldComponent;
+  let fixture: ComponentFixture<PasswordFieldComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PasswordFieldComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PasswordFieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/dynamic-form/components/password-field/password-field.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-field',
+  templateUrl: './password-field.component.html',
+  styleUrls: ['./password-field.component.scss'],
+})
+export class PasswordFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
