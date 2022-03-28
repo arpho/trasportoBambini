1196f42f17d56053af8d60410d6e0e9040a887a6
arpho <damicogiuseppe77@gmail.com>
2022-03-28 17:56:38 +0200

    question-password
    created  new question model

---
+++ src/app/modules/dynamic-form/models/question-password.ts
@@ -0,0 +1,14 @@
+import { QuestionBase } from "./question-base";
+import { QuestionProperties } from "./questionproperties";
+
+export interface passwordProperties extends QuestionProperties<string>{
+    repeatPassword?: boolean
+}
+
+export class PasswordQuestion extends QuestionBase<string>{
+    controlType = "password"
+    type:string
+    constructor(options:PasswordQuestion){
+        super(options)
+    }
+}
