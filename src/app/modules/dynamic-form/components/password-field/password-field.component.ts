5a02c96e7f200f9e0bb3925c99fc5ade9dd5ba31
arpho <damicogiuseppe77@gmail.com>
2022-03-28 19:25:10 +0200

    controlValueAccessor
    implemented interface

--- src/app/modules/dynamic-form/components/password-field/password-field.component.ts
+++ src/app/modules/dynamic-form/components/password-field/password-field.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import { StringMappingType } from 'typescript';
 
 @Component({
   selector: 'app-password-field',
   templateUrl: './password-field.component.html',
   styleUrls: ['./password-field.component.scss'],
 })
-export class PasswordFieldComponent implements OnInit {
+export class PasswordFieldComponent implements OnInit,ControlValueAccessor {
+  private onChange: Function = (password:string) => { };
+  // tslint:disable-next-line: ban-types
+  private onTouch: Function = () => { };
+  disabled: boolean;
+  password: string
+  repeatedPasword:string
 
   constructor() { }
+  writeValue(obj: any): void {
+    this.password= obj
+  }
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn) {
+    this.onTouch = fn;
+  }
+
+  
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
 
   ngOnInit() {}
 
