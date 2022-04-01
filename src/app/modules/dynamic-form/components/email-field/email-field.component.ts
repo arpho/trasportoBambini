1aefc29a1aebe8260fb5354f3ddfd1692a92b5c8
arpho <damicogiuseppe77@gmail.com>
2022-04-01 16:52:05 +0200

    controlValueAccessor
    implemented interface

--- src/app/modules/dynamic-form/components/email-field/email-field.component.ts
+++ src/app/modules/dynamic-form/components/email-field/email-field.component.ts
@@ -1,13 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-email-field',
   templateUrl: './email-field.component.html',
   styleUrls: ['./email-field.component.scss'],
 })
-export class EmailFieldComponent implements OnInit {
+export class EmailFieldComponent implements OnInit,ControlValueAccessor {
+
+
+  email:string
+  disabled: boolean;
+  touched: any;
+
+  set value (email:string){
+    this.email = email
+  }
+  private onChange: Function = (password: string) => { };
+  // tslint:disable-next-line: ban-types
+  private onTouch: Function = () => { };
+  private onValidationChange: any = () => { };
+
+  writeValue(pass: string): void {
+    this.email = pass
+  }
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn) {
+    this.onTouch = fn;
+  }
+
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouch();
+      this.touched = true;
+    }
+  }
+
+
 
   constructor() { }
+  
 
   ngOnInit() {}
 
