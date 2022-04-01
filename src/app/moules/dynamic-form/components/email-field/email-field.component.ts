3ea5806c1979f95e00fc057a057b18b89a74a46e
arpho <damicogiuseppe77@gmail.com>
2022-04-01 16:24:59 +0200

    email-field
    generated component

---
+++ src/app/moules/dynamic-form/components/email-field/email-field.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-field',
+  templateUrl: './email-field.component.html',
+  styleUrls: ['./email-field.component.scss'],
+})
+export class EmailFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
