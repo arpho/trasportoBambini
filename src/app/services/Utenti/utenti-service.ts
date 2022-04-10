022efa13df9e5d401fe6c875d194069cc95753f2
arpho <damicogiuseppe77@gmail.com>
2022-04-10 20:21:15 +0200

    refactoring
    service ready to read snapshot from firebase in web version 9
    #TODO: to be completed

--- src/app/services/Utenti/utenti-service.ts
+++ src/app/services/Utenti/utenti-service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Database, DatabaseReference, getDatabase, onValue, ref } from 'firebase/database';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Utenti } from 'src/app/models/utenti';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 import { ItemServiceInterface } from 'src/app/modules/item/models/ItemServiceInterface';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {configs} from '../../configs/credentials'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtentiService implements ItemServiceInterface {
 
-  constructor() { }
+    public utentiRef: DatabaseReference;
   categoriesService?: ItemServiceInterface;
   suppliersService?: ItemServiceInterface;
   paymentsService?: ItemServiceInterface;
   suppliersListRef?: any;
-  _items: BehaviorSubject<Utenti[]>;
-  items_list: Utenti[];
-  items: Observable<Utenti[]>;
+  db:Database
+  _items: BehaviorSubject<Array<Utenti>> = new BehaviorSubject([])
+  readonly items: Observable<Array<Utenti>> = this._items.asObservable()
+  items_list: Array<Utenti> = []
   getItem(key: string, next: () => void): void {
     throw new Error('Method not implemented.');
   }
@@ -31,5 +35,22 @@ export class UtentiService implements ItemServiceInterface {
   }
   createItem(item: Utenti) {
     throw new Error('Method not implemented.');
+  }
+
+  loadItems(){
+    onValue(this.utentiRef,(snapshot)=>{
+      console.log('got users',snapshot.val())
+    })
+
+  }
+
+  constructor() { 
+   getApps().length === 0 ?initializeApp(configs.firebase): getApp(); // check and initialize firebase app
+this.db = getDatabase()  
+this.utentiRef = ref(this.db,'userprofile/')
+
+
+
+
   }
 }
