8922e42a69af8faee025429a8537fdc7e04c6c27
arpho <damicogiuseppe77@gmail.com>
2022-04-12 20:11:09 +0200

    refactoring
    removed duplicated imports

--- src/app/modules/user/services/users.service.ts
+++ src/app/modules/user/services/users.service.ts
@@ -6,7 +6,6 @@ import { ItemServiceInterface } from "../../item/models/ItemServiceInterface";
 import { UserModel } from "../models/userModel";
 import { ItemModelInterface } from "../../item/models/itemModelInterface";
 import { BehaviorSubject, Observable } from 'rxjs';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 @Injectable({
   providedIn: "root"
