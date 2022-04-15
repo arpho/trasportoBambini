5f12a95e80a37874f292aac65003797c702fafac
arpho <damicogiuseppe77@gmail.com>
2022-04-15 16:16:42 +0200

    refactoring
     fixed bug, key must be serialized only if present, levwl now have as default value 3

--- src/app/models/utenti.spec.ts
+++ src/app/models/utenti.spec.ts
@@ -5,7 +5,8 @@ import { Utente } from './Utente';
 
 describe('testing users class',()=>{
     
-const data_no_key={firstName:'nome',lastName:'last',email:'email',dob:{day:13,month:5,year:1977},dor:{day:14,month:4,year:2022}}
+const data_no_key={firstName:'nome',lastName:'last',email:'email',dob:{day:13,month:5,year:1977},dor:{day:14,month:4,year:2022},
+telephones:[{numero:'1', note:'test'},{numero:'2',note:'test1'}]}
 
 const user= new Utente(data_no_key)    
 console.log('data',data_no_key)
@@ -19,6 +20,11 @@ console.log('data',data_no_key)
     expect(user.dor).toBeInstanceOf(DateModel)
     expect(user.dor.day).toEqual(data_no_key.dor.day)
     expect(new Utente().dor.day).toEqual(15)
+    expect(user.telephones.length).toEqual(2)
+    expect(user.telephones[0].numero).toEqual('1')
+    expect(user.telephones[0].note).toEqual('test')
+    console.log('key *#',user.serialize())
+    expect(user.serialize()['key']).toBeUndefined()
     
 
     }) 
