b9bb171161372940b0c95b4793320e89574b1c0a
arpho <damicogiuseppe77@gmail.com>
2022-04-19 18:01:17 +0200

    parentsService.getDummyItem
    implemented method

--- src/app/services/genitori/parents.service.ts
+++ src/app/services/genitori/parents.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Genitori } from 'src/app/models/genitori';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
+import { CustomersService } from '../customers/customers.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ParentsService {
+export class ParentsService extends CustomersService {
 
-  constructor() { }
+  getDummyItem(): ItemModelInterface {
+    return new Genitori()
+  }
+
+  constructor() {
+    super()
+   }
 }
