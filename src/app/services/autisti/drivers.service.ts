c9f36ae84e79738971d065f9e0dca26c31874886
arpho <damicogiuseppe77@gmail.com>
2022-04-19 17:58:25 +0200

    autistiService.getDummyItem
    implemented method

--- src/app/services/autisti/drivers.service.ts
+++ src/app/services/autisti/drivers.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Autista } from 'src/app/models/Addetti';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
+import { CustomersService } from '../customers/customers.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DriversService {
+export class DriversService extends CustomersService{
+  getDummyItem(): ItemModelInterface {
+    return new Autista()
+  }
 
-  constructor() { }
+  constructor() {
+    super()
+   }
 }
