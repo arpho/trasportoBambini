893ba22b75e66c84734767c6b4bd446532e0380d
arpho <damicogiuseppe77@gmail.com>
2022-04-23 20:02:47 +0200

    implementation
     implemented publishItems and loadData

--- src/app/services/vehicles/vehicles.service.ts
+++ src/app/services/vehicles/vehicles.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { DatabaseReference, getDatabase, ref } from 'firebase/database';
+import { DatabaseReference, getDatabase, onValue, ref } from 'firebase/database';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Vehicle } from 'src/app/models/vehicle';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
@@ -17,23 +17,43 @@ export class VehiclesService implements ItemServiceInterface{
   db=getDatabase()
   reference= 'vehicles'
   itemsListRef = ref(this.db,this.reference)
-  _items: BehaviorSubject<Vehicle[]>;
+  _items: BehaviorSubject<Vehicle[]>= new BehaviorSubject([])
   items_list: Vehicle[];
-  items: Observable<Vehicle[]>;
+  readonly items: Observable<Vehicle[]>= this._items.asObservable()
+
   constructor() { }
+
   getItem(key: string, next: (item?: any) => void): void {
     throw new Error('Method not implemented.');
   }
+
   updateItem(item: ItemModelInterface) {
     throw new Error('Method not implemented.');
   }
+
   deleteItem(key: string) {
     throw new Error('Method not implemented.');
   }
+
   getDummyItem(): ItemModelInterface {
     throw new Error('Method not implemented.');
   }
+
   createItem(item: ItemModelInterface) {
     throw new Error('Method not implemented.');
+  }
+
+  publishItems(lista:Vehicle[]){
+    this._items.next(lista)
+  }
+
+  loadData(){
+    onValue(this.itemsListRef,(snap)=>{
+      this.items_list = []
+      snap.forEach(item=>{
+        const vehicle = new Vehicle(item.val()).setKey(item.key)
+        this.items_list.push(vehicle)
+      })
+    })
   }
 }
