37068505c101a1b3a84cb5aa5bb3786f4cd9bdf3
arpho <damicogiuseppe77@gmail.com>
2022-04-23 19:19:51 +0200

    testing
    serialize works

--- src/app/models/vehicle.spec.ts
+++ src/app/models/vehicle.spec.ts
@@ -15,5 +15,13 @@ describe('Vehicle works correctly',()=>{
         expect(vehicle.modello).toEqual(VechicleDataTest.modello)
 
     })
+    it('serialization works',()=>{
+        expect(vehicle.serialize().key).toEqual(VechicleDataTest.key)
+        expect(vehicle.serialize().note).toEqual(VechicleDataTest.note)
+        expect(vehicle.serialize().title).toEqual(VechicleDataTest.title)
+        expect(vehicle.serialize().targa).toEqual(VechicleDataTest.targa)
+        expect(vehicle.serialize().marca).toEqual(VechicleDataTest.marca)
+        expect(vehicle.serialize().modello).toEqual(VechicleDataTest.modello)
+    })
 
 })
