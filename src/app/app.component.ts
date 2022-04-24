49156d6ec5b3229f7ca4c9da9c8306a4aeeed420
arpho <damicogiuseppe77@gmail.com>
2022-04-24 22:30:53 +0200

    refactoring
    implemented OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {initializeApp} from "firebase/app"
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { BehaviorSubject } from 'rxjs';
@@ -10,18 +10,21 @@ import { CustomersService } from './services/customers/customers.service';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
  
   public appPages =[]
   app = initializeApp(configs.firebase)
-  constructor(customers:CustomersService) {
+  constructor(public customers:CustomersService) {
+ 
+  }
+  ngOnInit(): void {
     const app = initializeApp(configs.firebase)
     const auth = getAuth()
     onAuthStateChanged(auth,(user)=>{
       if(user){
         console.log('user',user)
-        customers.items.subscribe(users=>{
+        this.customers.items.subscribe(users=>{
           if(users.length>0){
           const profilo = users.filter(u=>u.email==user.email)[0]
         if (profilo.level<3){
