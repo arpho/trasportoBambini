74147b3d052ca334e82b9273b52e161ff206d75b
arpho <damicogiuseppe77@gmail.com>
2022-04-24 22:52:36 +0200

    formatting
    formatted script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { ItemModule } from './modules/item/item.module';
 import { DynamicFormModule } from './modules/dynamic-form/dynamic-form.module';
 import { UserModule } from './modules/user/user.module';
-import {  HttpClientModule, HttpHandler } from '@angular/common/http';
+import { HttpClientModule, HttpHandler } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ItemModule,DynamicFormModule,UserModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: true,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerImmediately'
-})],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BrowserModule,HttpClientModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ItemModule,
+    DynamicFormModule,
+    UserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerImmediately'
+    })],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BrowserModule,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
