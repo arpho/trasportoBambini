d7d0479505ed03f1b3643d74b0a057adab30595e
arpho <damicogiuseppe77@gmail.com>
2022-04-25 19:29:46 +0200

    implementation

--- src/app/pages/Fleet/modificaVeicolo/update-vehicle/update-vehicle.page.ts
+++ src/app/pages/Fleet/modificaVeicolo/update-vehicle/update-vehicle.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { VehiclesService } from 'src/app/services/vehicles/vehicles.service';
+import { NuovoVeicoloPage } from '../../nuovo-veicolo/nuovo-veicolo.page';
 
 @Component({
   selector: 'app-update-vehicle',
   templateUrl: './update-vehicle.page.html',
   styleUrls: ['./update-vehicle.page.scss'],
 })
-export class UpdateVehiclePage implements OnInit {
+export class UpdateVehiclePage extends NuovoVeicoloPage {
 
-  constructor() { }
+  constructor(public modalController:ModalController,
+     public toastController:ToastController,
+     public service:VehiclesService
+
+
+    
+  ) {
+      super(modalController,service,toastController)
+     }
 
   ngOnInit() {
   }
 
-}
+
