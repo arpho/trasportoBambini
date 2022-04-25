b55ed767e0c81db9369fbdda6dd44f18be192744
arpho <damicogiuseppe77@gmail.com>
2022-04-25 15:48:33 +0200

    refactoring
    injected VehicleService

--- src/app/pages/Fleet/flotta/flotta.page.ts
+++ src/app/pages/Fleet/flotta/flotta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VehiclesService } from 'src/app/services/vehicles/vehicles.service';
 
 @Component({
   selector: 'app-flotta',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlottaPage implements OnInit {
 
-  constructor() { }
+  constructor(public service:VehiclesService) { }
 
   ngOnInit() {
   }
