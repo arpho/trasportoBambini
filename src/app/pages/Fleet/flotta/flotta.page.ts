04bb414606668b1301a03fb5cf808e7aca0d5f80
arpho <damicogiuseppe77@gmail.com>
2022-04-25 19:21:40 +0200

    implementing
    refactored flotta.page.hml and flotta.page.ts to show updateVehicle page

--- src/app/pages/Fleet/flotta/flotta.page.ts
+++ src/app/pages/Fleet/flotta/flotta.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VehiclesService } from 'src/app/services/vehicles/vehicles.service';
+import { UpdateVehiclePage } from '../modificaVeicolo/update-vehicle/update-vehicle.page';
 import { NuovoVeicoloPage } from '../nuovo-veicolo/nuovo-veicolo.page';
 
 @Component({
@@ -11,6 +12,7 @@ export class FlottaPage implements OnInit {
 
   constructor(public service: VehiclesService) { }
   public createModalPage = NuovoVeicoloPage
+  public editModalPage = UpdateVehiclePage
 
   ngOnInit() {
     this.service.loadData()
