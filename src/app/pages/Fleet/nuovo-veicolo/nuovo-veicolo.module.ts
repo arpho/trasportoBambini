2d9a0fd004b2b570b1958be4d96b402386b524f4
arpho <damicogiuseppe77@gmail.com>
2022-04-25 17:08:47 +0200

    importing
    imported DynamicFormModule

--- src/app/pages/Fleet/nuovo-veicolo/nuovo-veicolo.module.ts
+++ src/app/pages/Fleet/nuovo-veicolo/nuovo-veicolo.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { NuovoVeicoloPageRoutingModule } from './nuovo-veicolo-routing.module';
 
 import { NuovoVeicoloPage } from './nuovo-veicolo.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    DynamicFormModule,
     NuovoVeicoloPageRoutingModule
   ],
   declarations: [NuovoVeicoloPage]
