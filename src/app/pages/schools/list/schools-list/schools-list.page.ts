2a09ef2ec37f9fb95478ba06ae98444b50d607d5
arpho <damicogiuseppe77@gmail.com>
2022-04-26 17:16:19 +0200

    modalPage's schools
    defined parameters for the component

--- src/app/pages/schools/list/schools-list/schools-list.page.ts
+++ src/app/pages/schools/list/schools-list/schools-list.page.ts
@@ -3,7 +3,9 @@ import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterfa
 import { SchoolsService } from 'src/app/services/scuole/schools.service';
 import { servicesVersion } from 'typescript';
 import { NewSchoolPageModule } from '../../inserisciScuola/new-school/new-school.module';
+import { NewSchoolPage } from '../../inserisciScuola/new-school/new-school.page';
 import { UpdateSchoolPageModule } from '../../modificaScuola/update-school/update-school.module';
+import { UpdateSchoolPage } from '../../modificaScuola/update-school/update-school.page';
 
 @Component({
   selector: 'app-schools-list',
@@ -11,8 +13,8 @@ import { UpdateSchoolPageModule } from '../../modificaScuola/update-school/updat
   styleUrls: ['./schools-list.page.scss'],
 })
 export class SchoolsListPage implements OnInit {
-  editModalPage = UpdateSchoolPageModule
-  createModalPage = NewSchoolPageModule
+  editModalPage = UpdateSchoolPage
+  createModalPage = NewSchoolPage
   public filterFunction: (item: ItemModelInterface) => boolean;
 
   constructor(public service:SchoolsService) { }
