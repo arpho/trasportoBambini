c905d9eced4d65f0b5a5c910ae2154d96ca60fe8
arpho <damicogiuseppe77@gmail.com>
2022-04-26 17:42:41 +0200

    app-dynamic-form
    added component to the page and prepared contrtoller for handling it

--- src/app/pages/schools/inserisciScuola/new-school/new-school.page.ts
+++ src/app/pages/schools/inserisciScuola/new-school/new-school.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { School } from 'src/app/models/Schools';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { SchoolsService } from 'src/app/services/scuole/schools.service';
 
 @Component({
   selector: 'app-new-school',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-school.page.scss'],
 })
 export class NewSchoolPage implements OnInit {
+  school = new School()
+  schoolFields= [
+    new TextboxQuestion({
+      key:'denominazione',
+      label:'nome',
+      value:this.school.denominazione
+    })
+  ]
 
-  constructor() { }
+  filter(ev){
+    console.log('typing',ev)
+  }
+
+  submit(ev){
+    console.log('submit',ev)
+    this.school.load(ev)
+    console.log('new school',this.school)
+  }
+
+  constructor(public service:SchoolsService) { }
 
   ngOnInit() {
   }
