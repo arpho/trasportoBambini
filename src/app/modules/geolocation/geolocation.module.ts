73822735ab805389f7f2df7ae13bab3d8a30ca3b
arpho <damicogiuseppe77@gmail.com>
2022-04-28 19:09:20 +0200

    refactoring
    we instantiate address if undefined

--- src/app/modules/geolocation/geolocation.module.ts
+++ src/app/modules/geolocation/geolocation.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from './components/AddressField/address/address.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [AddressComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports:[AddressComponent]
 })
