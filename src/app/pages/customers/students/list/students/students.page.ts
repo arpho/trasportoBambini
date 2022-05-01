316079c262d3114835833e5ff2ac28af2af36287
arpho <damicogiuseppe77@gmail.com>
2022-05-01 19:10:05 +0200

    implementation
    impleted controller of students

--- src/app/pages/customers/students/list/students/students.page.ts
+++ src/app/pages/customers/students/list/students/students.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
+import { CustomersService } from 'src/app/services/customers/customers.service';
+import { StudentsService } from 'src/app/services/studenti/students.service';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.page.scss'],
 })
 export class StudentsPage implements OnInit {
+  public filterFunction: (item:ItemModelInterface) => boolean;
+  public filterFields: any[];
 
-  constructor() { }
+  constructor(public students: StudentsService) { }
 
   ngOnInit() {
+    this.filterFields = [
+      new TextboxQuestion({
+        key: 'title',
+        label: 'Filtra per argomento',
+        order: 1
+      }),
+      new TextboxQuestion({
+        key: 'note',
+        label: 'filtra per note',
+        order: 2
+      })
+    ];
   }
 
 }
