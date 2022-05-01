dab6744b477458ff0058e51aadf6a9917f78c436
arpho <damicogiuseppe77@gmail.com>
2022-05-01 17:34:41 +0200

    DynamicFormModule
    imported module

--- src/app/pages/schools/modificaScuola/update-school/update-school.module.ts
+++ src/app/pages/schools/modificaScuola/update-school/update-school.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { UpdateSchoolPageRoutingModule } from './update-school-routing.module';
 
 import { UpdateSchoolPage } from './update-school.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UpdateSchoolPageRoutingModule
+    UpdateSchoolPageRoutingModule,
+    DynamicFormModule
   ],
   declarations: [UpdateSchoolPage]
 })
--- src/app/pages/schools/modificaScuola/update-school/update-school.page.ts
+++ src/app/pages/schools/modificaScuola/update-school/update-school.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { School } from 'src/app/models/Schools';
+import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 import { SchoolsService } from 'src/app/services/scuole/schools.service';
 
 @Component({
@@ -11,19 +13,59 @@ import { SchoolsService } from 'src/app/services/scuole/schools.service';
 export class UpdateSchoolPage implements OnInit {
 
   title:string
+  school = new School()
+  schoolFields= [
+    new TextboxQuestion({
+      key:'denominazione',
+      label:'nome',
+      value:this.school.denominazione
+    }), new AddressQuestion({
+      key:'address',
+      label:'indirizzo',
+      value:this.school.address
+    })
+  ]
 
-  school:School
+  filter(ev){
+    console.log('typing',ev)
+  }
 
-  constructor(public navParams:NavParams,public modalCtrl:ModalController,public service:SchoolsService) { }
+  submit(ev){
+    console.log('submit',ev)
+    this.school.load(ev)
+    console.log('new school',this.school)
+    this.service.createItem(this.school).then((data)=>{
+      console.log('done',data)
+      this.school.setKey(data.key)
+
+      this.dismiss(this.school)
+    })
+  }
 
   dismiss(school?) {
     this.modalCtrl.dismiss(school)
   }
 
+  constructor(public navParams:NavParams,public modalCtrl:ModalController,public service:SchoolsService) { }
+
+ 
 
   ngOnInit() {
     this.school= this.navParams.get('item')
+    console.log('editing',this.school)
     this.title = this.school? `modifica scuola ${this.school.getTitle().value}`: 'modifica scuola'
+    this.schoolFields =  [
+      new TextboxQuestion({
+        key:'denominazione',
+        label:'nome',
+        value:this.school.denominazione
+      }), new AddressQuestion({
+        key:'address',
+        label:'indirizzo',
+        value:this.school.address
+      })
+    ]
+  
 
   }
 
