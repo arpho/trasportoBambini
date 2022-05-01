2229cd8110b8aa1c13de59fe81a9e16c00cf3010
arpho <damicogiuseppe77@gmail.com>
2022-05-01 18:38:15 +0200

    itemModule
    imported module in student's page

--- src/app/pages/customers/students/list/students/students.module.ts
+++ src/app/pages/customers/students/list/students/students.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { StudentsPageRoutingModule } from './students-routing.module';
 
 import { StudentsPage } from './students.page';
+import { ItemModule } from 'src/app/modules/item/item.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    StudentsPageRoutingModule
+    StudentsPageRoutingModule,
+    ItemModule
   ],
   declarations: [StudentsPage]
 })
