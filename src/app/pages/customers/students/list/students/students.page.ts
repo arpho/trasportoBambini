c8fd8fffa3ab260ffe9b4524d2cb3f1ce43ec1d9
arpho <damicogiuseppe77@gmail.com>
2022-05-01 19:14:52 +0200

    filterFunction
    implemented filter to show only students
    #TODO implement crud

--- src/app/pages/customers/students/list/students/students.page.ts
+++ src/app/pages/customers/students/list/students/students.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserType } from 'src/app/models/usersType';
+import { Utente } from 'src/app/models/Utente';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 import { CustomersService } from 'src/app/services/customers/customers.service';
@@ -16,6 +18,10 @@ export class StudentsPage implements OnInit {
   constructor(public students: StudentsService) { }
 
   ngOnInit() {
+
+    this.filterFunction = (utente:Utente)=>{
+      return utente.userType == UserType.studente
+    }
     this.filterFields = [
       new TextboxQuestion({
         key: 'title',
