130c30f1126471a9b86c1e6720159cc6f8dfb950
arpho <damicogiuseppe77@gmail.com>
2022-05-02 17:41:35 +0200

    refactoring
    added presentation 'time-date'

--- src/app/modules/dynamic-form/models/dateProperties.ts
+++ src/app/modules/dynamic-form/models/dateProperties.ts
@@ -1,6 +1,6 @@
 import { DateModel } from "../../user/models/birthDateModel";
 import { QuestionProperties } from "./questionproperties";
 
-export interface DateProperties extends QuestionProperties<DateModel>{
-    "presentation"?:'date'|'time'|'date-time'
+export interface DateProperties extends QuestionProperties<DateModel> {
+    "presentation"?: 'date' | 'time' | 'date-time' | 'trime-date'
 }
