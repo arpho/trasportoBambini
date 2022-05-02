9b39367217b0a47639ac03e21f008237aeca305b
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-02 13:06:35 +0200

    refactoriong
    presentation is optional

--- src/app/modules/dynamic-form/models/dateProperties.ts
+++ src/app/modules/dynamic-form/models/dateProperties.ts
@@ -2,5 +2,5 @@ import { DateModel } from "../../user/models/birthDateModel";
 import { QuestionProperties } from "./questionproperties";
 
 export interface DateProperties extends QuestionProperties<DateModel>{
-    "presentation":'date'|'time'|'date-time'
+    "presentation"?:'date'|'time'|'date-time'
 }
