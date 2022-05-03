b9b86eba09e88edc14ec8fa488e7b3d288328d19
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-03 11:15:57 +0200

    refactoring
    used MyToastService in update-school-page

--- src/app/pages/schools/modificaScuola/update-school/update-school.page.ts
+++ src/app/pages/schools/modificaScuola/update-school/update-school.page.ts
@@ -3,6 +3,7 @@ import { ModalController, NavParams, ToastController } from '@ionic/angular';
 import { School } from 'src/app/models/Schools';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { SchoolsService } from 'src/app/services/scuole/schools.service';
 
 @Component({
@@ -31,21 +32,13 @@ export class UpdateSchoolPage implements OnInit {
 
 
 
-  async presentToast(message:string) {
-    const toast = await this.toastController.create({
-      message: message,
-      position:'top',
-      duration: 2000
-    });
-    toast.present();
-  }
 
   submit(ev) {
     this.school.load(ev)
     this.service.updateItem(this.school).then((data) => {
-      this.presentToast('scuola modificata correttamente')
+      this.toastService.presentToast('scuola modificata correttamente')
     }).catch(error=>{
-      this.presentToast(' ci sono stati dei problemi')
+      this.toastService.presentToast(' ci sono stati dei problemi')
       console.error(error)
     }).finally(()=>{
       this.dismiss(this.school)
@@ -58,6 +51,7 @@ export class UpdateSchoolPage implements OnInit {
 
   constructor(public navParams: NavParams,
     public toastController:ToastController,
+    public toastService:MyToastService
     public modalCtrl: ModalController, public service: SchoolsService) { }
 
 
