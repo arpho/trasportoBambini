2c903e9cf89f1932dfdd48accfd7d45acd2253c8
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-03 11:12:44 +0200

    toastService
    implemented toastService

--- src/app/pages/schools/inserisciScuola/new-school/new-school.page.ts
+++ src/app/pages/schools/inserisciScuola/new-school/new-school.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import { School } from 'src/app/models/Schools';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { SchoolsService } from 'src/app/services/scuole/schools.service';
 
 @Component({
@@ -35,7 +36,12 @@ export class NewSchoolPage implements OnInit {
     this.service.createItem(this.school).then((data)=>{
       console.log('done',data)
       this.school.setKey(data.key)
-
+      this.toastService.presentToast(`scuola "${this.school.denominazione}" creata correttamente`)
+      
+    }).catch(error=>{
+      console.error(error)
+      this.toastService.presentToast("qualcosa è andato storto,riprova")
+    }).finally(()=>{
       this.dismiss(this.school)
     })
   }
@@ -45,7 +51,7 @@ export class NewSchoolPage implements OnInit {
   }
 
 
-  constructor(public service:SchoolsService,public modalCtrl:ModalController) { }
+  constructor(public service:SchoolsService,public modalCtrl:ModalController,public toastService:MyToastService) { }
 
   ngOnInit() {
   }
