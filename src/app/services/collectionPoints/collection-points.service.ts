6108203c5172ff5d5a5e79005dce19d2cfdf603e
arpho <damicogiuseppe77@gmail.com>
2022-05-03 20:16:22 +0200

    implementing
    started implementation of the observable items

--- src/app/services/collectionPoints/collection-points.service.ts
+++ src/app/services/collectionPoints/collection-points.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Database, DatabaseReference } from 'firebase/database';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CollectionPoint } from 'src/app/models/collectionPoints';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
+import { ItemServiceInterface } from 'src/app/modules/item/models/ItemServiceInterface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CollectionPointsService {
+export class CollectionPointsService implements ItemServiceInterface {
 
   constructor() { }
+  categoriesService?: ItemServiceInterface;
+  suppliersService?: ItemServiceInterface;
+  paymentsService?: ItemServiceInterface;
+  reference: string='collectionPoint'
+  _items: BehaviorSubject<CollectionPoint[]>=new BehaviorSubject([])
+  items_list: CollectionPoint[];
+  db: Database;
+  itemsListRef: DatabaseReference;
+  items: Observable<CollectionPoint[]>= this._items.asObservable()
+  getItem(key: string, next: (item?: any) => void): void {
+    throw new Error('Method not implemented.');
+  }
+  updateItem(item: CollectionPoint) {
+    throw new Error('Method not implemented.');
+  }
+  deleteItem(key: string) {
+    throw new Error('Method not implemented.');
+  }
+  getDummyItem(): CollectionPoint {
+    throw new Error('Method not implemented.');
+  }
+  createItem(item: CollectionPoint) {
+    throw new Error('Method not implemented.');
+  }
+  loadDataAndPublish(): void {
+    throw new Error('Method not implemented.');
+  }
 }
