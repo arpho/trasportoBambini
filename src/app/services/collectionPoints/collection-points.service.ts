f7be093d092666dda54ac92020d9193081bd7337
arpho <damicogiuseppe77@gmail.com>
2022-05-03 20:41:51 +0200

    implementing

--- src/app/services/collectionPoints/collection-points.service.ts
+++ src/app/services/collectionPoints/collection-points.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Database, DatabaseReference } from 'firebase/database';
+import { Database, DatabaseReference, DataSnapshot, getDatabase, onValue, ref} from 'firebase/database';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CollectionPoint } from 'src/app/models/collectionPoints';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
@@ -17,8 +17,8 @@ export class CollectionPointsService implements ItemServiceInterface {
   reference: string='collectionPoint'
   _items: BehaviorSubject<CollectionPoint[]>=new BehaviorSubject([])
   items_list: CollectionPoint[];
-  db: Database;
-  itemsListRef: DatabaseReference;
+  db: Database= getDatabase()
+  itemsListRef: DatabaseReference= ref(this.db,this.reference)
   items: Observable<CollectionPoint[]>= this._items.asObservable()
   getItem(key: string, next: (item?: any) => void): void {
     throw new Error('Method not implemented.');
@@ -36,6 +36,15 @@ export class CollectionPointsService implements ItemServiceInterface {
     throw new Error('Method not implemented.');
   }
   loadDataAndPublish(): void {
-    throw new Error('Method not implemented.');
+    onValue(this.itemsListRef,(DataSnapshot)=>{
+      DataSnapshot.forEach(e=>{
+        const item = new CollectionPoint(e.val()).setKey(e.key)
+      })
+    })
+  }
+  publishItems(lista: CollectionPoint[]) {// must stay inside onValue to update data evry time there is an update
+
+    this._items.next(lista)
+
   }
 }
