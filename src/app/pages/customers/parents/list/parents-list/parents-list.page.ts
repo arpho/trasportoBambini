bacf43f29449bd0ed8c6be916ff806b07963f38f
arpho <damicogiuseppe77@gmail.com>
2022-05-05 19:20:58 +0200

    modalPage
    defined modal pages,
    #TODO: declare pages

--- src/app/pages/customers/parents/list/parents-list/parents-list.page.ts
+++ src/app/pages/customers/parents/list/parents-list/parents-list.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserType } from 'src/app/models/usersType';
 import { Utente } from 'src/app/models/Utente';
 import { ParentsService } from 'src/app/services/genitori/parents.service';
+import { NewParentPage } from '../../create/new-parent/new-parent.page';
+import { UpdateParentPage } from '../../edit/update-parent/update-parent.page';
 
 @Component({
   selector: 'app-parents-list',
@@ -9,8 +11,11 @@ import { ParentsService } from 'src/app/services/genitori/parents.service';
   styleUrls: ['./parents-list.page.scss'],
 })
 export class ParentsListPage implements OnInit {
+  createModalpage = NewParentPage
+  editModalPage = UpdateParentPage
   filterFunction: (utente: Utente) => boolean;
   ngOnInit(): void {
+
     this.filterFunction = (utente:Utente)=>{
       return utente.userType == UserType.genitore
     }
