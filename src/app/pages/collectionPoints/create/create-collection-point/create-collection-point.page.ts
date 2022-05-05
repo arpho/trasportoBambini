f47730e828dfcf7f8bb851c40866386fde6c3e27
arpho <damicogiuseppe77@gmail.com>
2022-05-05 15:44:00 +0200

    refadtoring
     a better implementation

--- src/app/pages/collectionPoints/create/create-collection-point/create-collection-point.page.ts
+++ src/app/pages/collectionPoints/create/create-collection-point/create-collection-point.page.ts
@@ -4,6 +4,7 @@ import { CollectionPoint } from 'src/app/models/collectionPoints';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { TextAreaBox } from 'src/app/modules/dynamic-form/models/question-textArea';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { CollectionPointsService } from 'src/app/services/collectionPoints/collection-points.service';
 
 @Component({
@@ -31,14 +32,29 @@ export class CreateCollectionPointPage implements OnInit {
     submit(ev){
       console.log('submit',ev)
       const collectionPoint = new CollectionPoint(ev)
+      let error: Error
+      let result:CollectionPoint
       console.log('collection point',collectionPoint)
+      this.toaster.presentToast('punto di raccolta creato correttamente')
+
+      this.service.createItem(collectionPoint).then(point=>{
+        result = collectionPoint.setKey(point.key)
+        console.log('created',point)
+      }).catch(_error=>{
+        console.error(_error)
+        error = _error
+        this.toaster.presentToast('ho riscontrato dei problemi, riprova')
+      }).finally(()=>{
+        this.dismiss(result)
+
+      })
     }
 
     dismiss(point?) {
       this.modalCtrl.dismiss(point)
     }
 
-  constructor(public service:CollectionPointsService,public modalCtrl:ModalController) { }
+  constructor(public service:CollectionPointsService,public modalCtrl:ModalController,public toaster:MyToastService) { }
 
   ngOnInit() {
   }
