7611f9c22b6d1fe37b2d32e14202f7f2d44fd23a
arpho <damicogiuseppe77@gmail.com>
2022-05-06 19:49:11 +0200

    refactoring
    implemented code to  handle form

--- src/app/pages/customers/parents/create/new-parent/new-parent.page.ts
+++ src/app/pages/customers/parents/create/new-parent/new-parent.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Genitore } from 'src/app/models/genitore';
+import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { CustomersService } from 'src/app/services/customers/customers.service';
 
 @Component({
   selector: 'app-new-parent',
@@ -6,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-parent.page.scss'],
 })
 export class NewParentPage implements OnInit {
+  parent = new Genitore()
+  public formFields = [
+    new TextboxQuestion({key:'firstName',label:'nome',value:this.parent.firstName}),
+    new TextboxQuestion({key:'lastName',label:'Cognome',value:this.parent.lastName}),
+    new AddressQuestion({key:'indirizzo',label:'indirizzo',value:this.parent.indirizzo})
+]
 
-  constructor() { }
+
+  filter(ev){
+    console.log('typing',ev)
+  }
+
+  submit(ev){
+    this.parent.load(ev)
+    console.log('submit',ev,this.parent)
+
+  }
+
+  constructor(public service:CustomersService) { }
 
   ngOnInit() {
   }
