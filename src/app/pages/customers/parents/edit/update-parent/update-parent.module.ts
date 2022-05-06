ccb6dc75bfa54c79b6b5ab7bf693fbfd1f31043c
arpho <damicogiuseppe77@gmail.com>
2022-05-06 22:31:06 +0200

    refactoring
    imported DynamicFormModule

--- src/app/pages/customers/parents/edit/update-parent/update-parent.module.ts
+++ src/app/pages/customers/parents/edit/update-parent/update-parent.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { UpdateParentPageRoutingModule } from './update-parent-routing.module';
 
 import { UpdateParentPage } from './update-parent.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UpdateParentPageRoutingModule
+    UpdateParentPageRoutingModule,
+    DynamicFormModule
   ],
   //declarations: [UpdateParentPage]
 })
--- src/app/pages/customers/parents/edit/update-parent/update-parent.page.ts
+++ src/app/pages/customers/parents/edit/update-parent/update-parent.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Genitore } from 'src/app/models/genitore';
+import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
+import { CustomersService } from 'src/app/services/customers/customers.service';
 
 @Component({
   selector: 'app-update-parent',
@@ -7,15 +12,49 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./update-parent.page.scss'],
 })
 export class UpdateParentPage implements OnInit {
+  parent = new Genitore()
+  title
 
-  dismiss(vehicle?) {
-    this.modalCtrl.dismiss(vehicle)
+  public formFields = [
+    new TextboxQuestion({key:'firstName',label:'nome',value:this.parent.firstName}),
+    new TextboxQuestion({key:'lastName',label:'Cognome',value:this.parent.lastName}),
+    new AddressQuestion({key:'indirizzo',label:'indirizzo',value:this.parent.address})
+]
+
+
+  filter(ev){
+    console.log('typing',ev)
   }
 
+  submit(ev){
+    let result:Genitore
+    this.parent.load(ev)
+    console.log('submit',ev,this.parent)
 
-  constructor(public modalCtrl:ModalController) { }
+    this.service.updateItem(this.parent).then(item=>{
+    this.toaster.presentToast('genitore modificato correttamente')
+    }).catch(error=>{
+      console.error(error)
+      this.toaster.presentToast('ho riscontrato dei problemi, riprova')
+    }).finally(()=>{
+      this.dismiss(result)
+    })
+  }
+
+  dismiss(parent?) {
+    this.modalCtrl.dismiss(parent)
+  }
+
+  constructor(public modalCtrl:ModalController,
+    public service:CustomersService,
+    public toaster:MyToastService,
+    public navParams:NavParams
+    ) { }
 
   ngOnInit() {
+    this.parent = this.navParams.get('item')
+    this.title= `modifica ${this.parent.getTitle().value}`
+
   }
 
 }
