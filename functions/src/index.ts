c2e6f60f22afd97dd1f55b27f69a261c272ec074
arpho <damicogiuseppe77@gmail.com>
2022-05-07 10:16:48 +0200

    cloud function
    add admin custom claim, lint does not work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
 "use strict";
- import * as functions from "firebase-functions";
- import * as admin from "firebase-admin";
- admin.initializeApp();
- exports.adAddminRole = functions.https.onCall((data,context)=>{
-     // get user and add custom claim (admin)
-     return admin.auth().getUserByEmail(data.email).then(user=>{
-         return admin.auth().setCustomUserClaims(user.uid,{
-             admin:true
-         }).then(()=>{
-             return {
-                 message:` Success! ${data.email} as been made an admin`
-             }
-         }).catch(err=>{
-             return err
-         })
-     })
- })
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+exports.adAddminRole = functions.https.onCall((data) => {
+	// get user and add custom claim (admin)
+	return admin.auth().getUserByEmail(data.email).then(user => {
+		return admin.auth().setCustomUserClaims(user.uid, {
+			admin: true
+		}).then(() => {
+			return {
+				message: ` Success! ${data.email} as been made an admin`
+			}
+		}).catch(err => {
+			return err
+		})
+	})
+})
