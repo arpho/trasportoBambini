95e1c9d9ec3f5b025bfeff1538d15caf5b92e0f3
arpho <damicogiuseppe77@gmail.com>
2022-05-09 01:55:06 +0200

    refactoring
    geodecode button showed only if the form has enough data

--- src/app/modules/geolocation/components/AddressField/address/address.component.ts
+++ src/app/modules/geolocation/components/AddressField/address/address.component.ts
@@ -6,184 +6,180 @@ import { ValueAccessor } from '@ionic/angular/directives/control-value-accessors
 import { Subscription } from 'rxjs';
 import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { Address } from '../../../models/Address';
- //import {  } from 'google-maps';
+//import {  } from 'google-maps';
 
 
 
 @Component({
-  selector: 'app-address',
-  templateUrl: './address.component.html',
-  styleUrls: ['./address.component.scss'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    multi: true,
-    useExisting: AddressComponent
-  },]
+	selector: 'app-address',
+	templateUrl: './address.component.html',
+	styleUrls: ['./address.component.scss'],
+	providers: [{
+		provide: NG_VALUE_ACCESSOR,
+		multi: true,
+		useExisting: AddressComponent
+	},]
 })
 export class AddressComponent implements OnInit, ControlValueAccessor, OnDestroy {
-  @Input() address: Address = new Address()
+	@Input() address: Address = new Address()
 
-  addressForm
-  disabled = false
+	addressForm
+	disabled = false
+	canGeodecode = false
+	private onChange: Function = (password: string) => { };
+	// tslint:disable-next-line: ban-types
+	private onTouch: Function = () => { };
+	touched = false;
+	subscription: Subscription
+	canGeocode = false
+	constructor(
+		public formBuilder: FormBuilder,
+		public toaster: MyToastService,
+		// public geocoder:google,
+		mapsAPILoader: MapsAPILoader,) { }
+	protected injector: Injector;
+	protected el: ElementRef<any>;
+	protected lastValue: any;
+	writeValue(value: Address): void {
+		this.address = value
+	}
 
-  private onChange: Function = (password: string) => { };
-  // tslint:disable-next-line: ban-types
-  private onTouch: Function = () => { };
-  touched = false;
-  subscription: Subscription
-  canGeocode= false
-  constructor(
-	  public formBuilder: FormBuilder,
-	public toaster:MyToastService,
-    // public geocoder:google,
-    mapsAPILoader: MapsAPILoader,) { }
-  protected injector: Injector;
-  protected el: ElementRef<any>;
-  protected lastValue: any;
-  writeValue(value: Address): void {
-    this.address = value
-  }
+
+	registerOnChange(fn) {
+		this.onChange = fn;
+	}
+	registerOnTouched(fn) {
+		this.onTouch = fn;
+	}
+	fetchAddress(location) {
+		const address = new Address()
+		console.log('fetching', location)
+		location['address_components'].forEach(element => {
+			console.log('components', element)
+			if (element.types[0] == 'street_number') {
+				address.number = element.short_name
+			}
+			if (element.types[0] == 'route') {
+				address.street = element.short_name
+			}
+			if (element.types[0] == 'locality') {
+				address.city = element.short_name
+
+			}
+
+			if (element.types[0] == 'administrative_area_level_2') {
+				address.province = element.short_name
+			}
+
+			if (element.types[0] == 'postal_code') {
+				address.cap = element.short_name
+			}
 
 
-  registerOnChange(fn) {
-    this.onChange = fn;
-  }
-  registerOnTouched(fn) {
-    this.onTouch = fn;
-  }
-  fetchAddress(location){
-    const address = new Address()
-    console.log('fetching',location)
-    location['address_components'].forEach(element => {
-      console.log('components',element)
-      if(element.types[0]=='street_number')
-      {
-        address.number = element.short_name
-      }
-      if(element.types[0]=='route')
-      {
-        address.street = element.short_name
-      }
-      if(element.types[0]=='locality')
-      {
-        address.city= element.short_name
+		});
+		console.log('address', address)
+		return address
+	}
 
-      }
+	setAddressForm(address: Address) {
+		console.log('setting', address)
+		this.addressForm.controls.number.setValue(address.number)
+		this.addressForm.controls.cap.setValue(address.cap)
+		this.addressForm.controls.city.setValue(address.city)
+		this.addressForm.controls.province.setValue(address.province)
+		this.addressForm.controls.street.setValue(address.street)
+	}
 
-      if(element.types[0]=='administrative_area_level_2')
-        {
-          address.province = element.short_name
-      }
+	localize() {
+		console.log('localizing')
+		navigator.geolocation.getCurrentPosition((position: Position) => {
+			console.log('position', position)
+			this.address.latitude = position.coords.latitude
+			this.addressForm.controls.longitude.setValue(position.coords.longitude)
+			this.addressForm.controls.latitude.setValue(position.coords.latitude)
+			this.address.longitude = position.coords.longitude
+			let geodecoder = new google.maps.Geocoder()
+			let latlng = { lat: position.coords.latitude, lng: position.coords.longitude }
+			geodecoder.geocode({ 'location': latlng }, results => {
+				console.log('results', results[0])
+				this.address = this.fetchAddress(results[0])
+				this.setAddressForm(this.address)
 
-      if(element.types[0]=='postal_code')
-      {
-        address.cap= element.short_name
-      }
+			})
+		})
+	}
 
-      
-    });
-    console.log('address',address)
-    return address
-  }
+	geodecode() {
+		console.log('geodecoding')
+		const address = new Address(this.addressForm.value)
+		console.log(address.fetchAddress())
+		let geodecoder = new google.maps.Geocoder()
+		geodecoder.geocode({ address: this.address.fetchAddress() }, (response) => {
+			console.log('ciao', response)
+			if (response) {
+				console.log('lat', response[0].geometry.location.lat())
+				console.log('lng', response[0].geometry.location.lng())
+				this.addressForm.controls.latitude.setValue(response[0].geometry.location.lat())
+				this.addressForm.controls.longitude.setValue(response[0].geometry.location.lng())
+			}
+			else {
+				this.toaster.presentToast("non sono riuscito a ricavare le coordinate dell'indirizzo che mi hai fornito")
+			}
+		})
 
-  setAddressForm(address:Address){
-	  console.log('setting',address)
-    this.addressForm.controls.number.setValue(address.number)
-    this.addressForm.controls.cap.setValue(address.cap)
-    this.addressForm.controls.city.setValue(address.city)
-    this.addressForm.controls.province.setValue(address.province)
-    this.addressForm.controls.street.setValue(address.street)
-  }
+	}
 
-  localize(){
-    console.log('localizing')
-    navigator.geolocation.getCurrentPosition((position:Position)=>{
-      console.log('position',position)
-      this.address.latitude = position.coords.latitude
-      this.addressForm.controls.longitude.setValue( position.coords.longitude)
-      this.addressForm.controls.latitude.setValue(position.coords.latitude)
-      this.address.longitude= position.coords.longitude
-      let geodecoder = new google.maps.Geocoder()
-      let latlng ={ lat:position.coords.latitude,lng:position.coords.longitude}
-       geodecoder.geocode({'location':latlng},results=>{
-        console.log('results',results[0])
-       this.address = this.fetchAddress(results[0])
-       this.setAddressForm(this.address)
-
-      }) 
-    })
-  }
-
-  geodecode(){
-	  console.log('geodecoding')
-	  const address = new Address(this.addressForm.value)
-	  console.log(address.fetchAddress())
-	  let geodecoder = new google.maps.Geocoder()
-	  geodecoder.geocode({address:this.address.fetchAddress()},(response)=>{
-		  console.log('ciao',response)
-		  if(response){
-		  console.log('lat',response[0].geometry.location.lat())
-		  console.log('lng',response[0].geometry.location.lng())
-		  this.addressForm.controls.latitude.setValue(response[0].geometry.location.lat())
-		  this.addressForm.controls.longitude.setValue(response[0].geometry.location.lng())
+	markAsTouched() {
+		if (!this.touched) {
+			this.onTouch();
+			this.touched = true;
 		}
-		else{
-			this.toaster.presentToast("non sono riuscito a ricavare le coordinate dell'indirizzo che mi hai fornito")
+	}
+
+	handleChangeEvent(el: HTMLElement, value: any): void {
+		throw new Error('Method not implemented.');
+	}
+	_handleBlurEvent(el: any): void {
+		throw new Error('Method not implemented.');
+	}
+	setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled
+	}
+	ngOnDestroy(): void {
+		if (this.subscription) {
+			this.subscription.unsubscribe()
 		}
-	  })
-
-  }
-
-  markAsTouched() {
-    if (!this.touched) {
-      this.onTouch();
-      this.touched = true;
-    }
-  }
-
-  handleChangeEvent(el: HTMLElement, value: any): void {
-    throw new Error('Method not implemented.');
-  }
-  _handleBlurEvent(el: any): void {
-    throw new Error('Method not implemented.');
-  }
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled
-  }
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe()
-    }
-  }
-  ngAfterViewInit(): void {
-  }
+	}
+	ngAfterViewInit(): void {
+	}
 
 
-  ngOnInit() {
-    if(this.address){
-    this.addressForm = this.formBuilder.group({
-      street: new FormControl(this.address.street),
-      cap: new FormControl(this.address.cap),
-      city: new FormControl(this.address.city),
-      longitude: new FormControl(this.address.longitude),
-      latitude: new FormControl(this.address.latitude),
-      province: new FormControl(this.address.province),
-      number: new FormControl(this.address.number)
-    })
-    this.subscription = this.addressForm.valueChanges.subscribe(d => {
-	console.log('form',d)
-	this.address.load(d)
-	
-      this.markAsTouched()
-      this.onChange(d)
-    })}
-    
+	ngOnInit() {
+		if (this.address) {
+			this.addressForm = this.formBuilder.group({
+				street: new FormControl(this.address.street),
+				cap: new FormControl(this.address.cap),
+				city: new FormControl(this.address.city),
+				longitude: new FormControl(this.address.longitude),
+				latitude: new FormControl(this.address.latitude),
+				province: new FormControl(this.address.province),
+				number: new FormControl(this.address.number)
+			})
+			this.subscription = this.addressForm.valueChanges.subscribe(d => {
+				console.log('form', d)
+				this.address.load(d)
+				this.canGeodecode = !!d['street'] && !!d["city"] && !! d['number']
+				this.markAsTouched()
+				this.onChange(d)
+			})
+		}
 
-  }
+
+	}
 
 
 
 
-  onTouched: any = () => { };
+	onTouched: any = () => { };
 
 }
