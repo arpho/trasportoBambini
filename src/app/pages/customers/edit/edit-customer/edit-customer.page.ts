c477e34c28d1ffc4722a420b7ef475f686c603a4
arpho <damicogiuseppe77@gmail.com>
2022-05-10 18:07:18 +0200

    refactoring
    we can set enabled claims

--- src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
+++ src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
@@ -3,6 +3,7 @@ import { NavParams } from '@ionic/angular';
 import { Utente } from 'src/app/models/Utente';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { SwitchQuestion } from 'src/app/modules/item/models/question-switch';
 import { CustomersService } from 'src/app/services/customers/customers.service';
 
@@ -17,7 +18,8 @@ export class EditCustomerPage implements OnInit {
   formFields:any[]
 
   constructor(public navParams:NavParams,
-    public service:CustomersService) { }
+    public service:CustomersService,
+	public toaster:MyToastService) { }
 
   ngOnInit() {
 this.utente = this.navParams.get('item')
@@ -26,7 +28,7 @@ this.formFields= [
   new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.utente.firstName }),
   new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.utente.lastName }),
   new AddressQuestion({ key: 'indirizzo', label: 'indirizzo', value: this.utente.address }),
-new SwitchQuestion({key:'enabled',label:'Utente abilitato',value:this.utente.enabled,iconTrue:'checkmark',iconFalse:'close',labelTrue:'utente abilitato',labelFalse:'utente non abilitato'})]
+  new SwitchQuestion({key:'enabled',label:'Utente abilitato',value:this.utente.enabled,iconTrue:'checkmark',iconFalse:'close',labelTrue:'utente abilitato',labelFalse:'utente non abilitato'})]
 
   }
 
@@ -35,7 +37,14 @@ new SwitchQuestion({key:'enabled',label:'Utente abilitato',value:this.utente.ena
     this.utente.load(ev)
     console.log('utente',this.utente)
     let data = {email:this.utente.email,claim:{'enabled':this.utente.enabled}}
-    this.service.adAddCustomClaim(data)
+	console.log('claim',data)
+    this.service.adAddCustomClaim(data).catch((error)=>{
+		console.error(error)
+		this.toaster.presentToast('ho riscontrto degli errori')
+	}).then((msg)=>{
+		console.log('claims set ',msg)
+this.toaster.presentToast('claim impostato correttamente')
+	})
   }
   submit(ev){
     this.utente.load(ev)
