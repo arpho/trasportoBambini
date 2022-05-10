66478e1d9e7ccd1a19a25c7914db10933cfd06eb
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-10 11:23:46 +0200

    dyanamicForm
    page ready to display  the form

--- src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
+++ src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams } from '@ionic/angular';
+import { Utente } from 'src/app/models/Utente';
+import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { CustomersService } from 'src/app/services/customers/customers.service';
 
 @Component({
   selector: 'app-edit-customer',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-customer.page.scss'],
 })
 export class EditCustomerPage implements OnInit {
+  utente: Utente
+  title:string
+  formFields:any[]
 
-  constructor() { }
+  constructor(public navParams:NavParams,
+    public service:CustomersService) { }
 
   ngOnInit() {
+this.utente = this.navParams.get('item')
+this.title = `modifica ${this.utente.getTitle().value}`
+this.formFields= [
+  new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.utente.firstName }),
+  new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.utente.lastName }),
+  new AddressQuestion({ key: 'indirizzo', label: 'indirizzo', value: this.utente.address })]
+
+  }
+
+  filter(ev){
+    console.log('editing',ev)
+    this.utente.load(ev)
+    console.log('utente',this.utente)
+  }
+  submit(ev){
+    this.utente.load(ev)
+    console.log('submitting',this.utente)
   }
 
 }
