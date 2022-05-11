13b8048d0659ee4d90e7cd4c568a1280216ddf54
arpho <damicogiuseppe77@gmail.com>
2022-05-11 20:01:49 +0200

    refactoring
    added form's fields for uder's role and type

--- src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
+++ src/app/pages/customers/edit/edit-customer/edit-customer.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { configs } from 'src/app/configs/configs';
 import { Utente } from 'src/app/models/Utente';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
+import { DropdownQuestion } from 'src/app/modules/dynamic-form/models/question-dropdown';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { SwitchQuestion } from 'src/app/modules/item/models/question-switch';
@@ -24,13 +26,17 @@ export class EditCustomerPage implements OnInit {
 
   ngOnInit() {
 this.utente = this.navParams.get('item')
+console.log('utente 2 edit',this.utente)
 this.title = `modifica ${this.utente.getTitle().value}`
 this.formFields= [
   new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.utente.firstName }),
   new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.utente.lastName }),
   new AddressQuestion({ key: 'indirizzo', label: 'indirizzo', value: this.utente.address }),
-  new SwitchQuestion({key:'enabled',label:'Utente abilitato',value:this.utente.enabled,iconTrue:'checkmark',iconFalse:'close',labelTrue:'utente abilitato',labelFalse:'utente non abilitato'})]
-
+  new SwitchQuestion({key:'enabled',label:'Utente abilitato',value:this.utente.enabled,iconTrue:'checkmark',
+  iconFalse:'close',labelTrue:'utente abilitato',labelFalse:'utente non abilitato'}),
+  new DropdownQuestion({key:"userType",label:"categoria utente",options:configs.customerType,value:this.utente.userType}),
+  new DropdownQuestion({key:"level",label:"ruolo",options:configs.accessLevel,value:this.utente.level})
+]
   }
 
   dismiss(vehicle?) {
@@ -44,22 +50,64 @@ this.formFields= [
     console.log('utente',this.utente)
     let data = {email:this.utente.email,claim:{'enabled':this.utente.enabled}}
 	console.log('claim',data)
-    this.service.adAddCustomClaim(data).catch((error)=>{
-		console.error(error)
-		this.toaster.presentToast('ho riscontrto degli errori')
-	}).then((msg)=>{
-		console.log('claims set ',msg)
-this.toaster.presentToast('claim impostato correttamente')
-	})
   }
+
+  setClaim(data:{email:string,claim:{}},successMessage:string){
+	this.service.adAddCustomClaim(data).catch((error)=>{
+		console.error(error)
+		this.toaster.presentToast('ho riscontrato degli errori')
+	}).then((msg)=>{
+		console.log(`claims set for ${ Object.keys(data.claim)} `,msg)
+this.toaster.presentToast(successMessage)
+	})
+
+  }
+
+  claimEnabled(b:boolean){
+	let data = {email:this.utente.email,claim:{'enabled':b}}
+	this.setClaim(data, "claim enabled impostato correttamente")
+  }
+
+  claimUserType(tipo:number,userTYpe:string){
+	  console.log('setting',userTYpe,tipo)
+	let data = {email:this.utente.email,claim:{"userType":tipo}}
+	this.setClaim(data,`utente  impostato come  ${userTYpe} `)
+	
+  }
+  claimUserLevel(roleValue:number,roleKey:string){
+	let data = {email:this.utente.email,claim:{role:roleValue}}
+	this.setClaim(data,`utente ${this.utente.getTitle().value} impostato come ${roleKey}`)
+	
+  }
+
+
   submit(ev){
+	  let result:Utente
     this.utente.load(ev)
     console.log('submitting',this.utente)
 	this.service.updateItem(this.utente).then(()=>{
-		this.toaster.presentToast(`l'utente ${this.utente.getTitle().value} è stato aggiornato`).catch((err)=>{
-			console.error(err)
-			this.toaster.presentToast("qualcosa nonè andato per il verso giusto")
-		})
+		result = this.utente
+		this.toaster.presentToast(`l'utente ${this.utente.getTitle().value} è stato aggiornato`);
+
+		this.claimEnabled(this.utente.enabled)
+		const fetchUserType = (type:number)=>{
+			return configs.customerType.filter((e)=>{
+				return e.value==type
+			})[0].key
+		}
+		const fetchUserlevel = (role:number)=>{
+			return configs.accessLevel.filter((e)=>{
+				return e.value==role
+			})[0].key
+		}
+		this.claimUserType(this.utente.userType,fetchUserType(this.utente.userType))
+		this.claimUserLevel(this.utente.level,fetchUserlevel(this.utente.level))
+		
+		
+
+
+	}).finally(()=>{
+		this.dismiss(result)
 	})
   }
 
