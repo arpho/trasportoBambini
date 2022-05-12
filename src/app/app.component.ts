f6ee75536d6cec76e7ee4c635b621a0c3b4605ac
arpho <damicogiuseppe77@gmail.com>
2022-05-12 16:23:22 +0200

    refactoring
    replaced ' with "

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {initializeApp} from "firebase/app"
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { BehaviorSubject } from 'rxjs';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { BehaviorSubject } from "rxjs";
 import {configs} from "./configs/credentials"
-import { Utente } from './models/Utente';
-import { CustomersService } from './services/customers/customers.service';
+import { Utente } from "./models/Utente";
+import { CustomersService } from "./services/customers/customers.service";
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent implements OnInit {
 
@@ -25,20 +25,21 @@ export class AppComponent implements OnInit {
 
 
 		const token = await user.getIdTokenResult(true)
-		console.log('user ok è',user)
-		console.log('token.claims',token.claims)
+		console.log("user ok è",user)
+		console.log("token.claims",token.claims)
       if(user){
 		const token = await user.getIdTokenResult(true)
-		console.log('claims enabled',token.claims.enabled)
-		console.log('role claim',token['role'])
+		console.log("claims enabled",token.claims.enabled)
+		console.log("role claim",token.claims["role"])
+		console.log("usertype claim",token.claims["userType"])
      
 			if(token.claims.enabled)
-         {  this.appPages= [   { title: 'utenti', url: '/customers', icon: 'people' },
-          { title: 'pulmini', url: '/flotta', icon: 'bus' },
-        {title:'Scuole',url:'/schools-list',icon:'business'},
-      {title:'Studenti',url:'/students',icon:'happy'},
-    {title:'punti di raccolta', url:'collection-points-list',icon:'location'},
-    {title:'Genitori', url:'/parents-list',icon:'body'}]}
+         {  this.appPages= [   { title: "utenti", url: "/customers", icon: "people" },
+          { title: "pulmini", url: "/flotta", icon: "bus" },
+        {title:"Scuole",url:"/schools-list",icon:"business"},
+      {title:"Studenti",url:"/students",icon:"happy"},
+    {title:"punti di raccolta", url:"collection-points-list",icon:"location"},
+    {title:"Genitori", url:"/parents-list",icon:"body"}]}
 		  
 		
 	  }
