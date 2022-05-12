f82624bc481d84f76b7a38c9c7940e5c227ef62b
arpho <damicogiuseppe77@gmail.com>
2022-05-12 20:04:57 +0200

    google-map
    added options for google-maps

--- src/app/modules/geolocation/components/show-tracking/show-tracking.component.ts
+++ src/app/modules/geolocation/components/show-tracking/show-tracking.component.ts
@@ -9,6 +9,32 @@ export class ShowTrackingComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  zoom = 12
+  center: google.maps.LatLngLiteral
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
+
+  ngOnInit() {
+	navigator.geolocation.getCurrentPosition((position) => {
+		this.center = {
+		  lat: position.coords.latitude,
+		  lng: position.coords.longitude,
+		}
+	  })
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--
+  }
 
 }
