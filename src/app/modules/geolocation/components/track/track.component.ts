989e4ec9ac74b06cd111c6bd85cd4bc39f3e871d
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-13 15:58:27 +0200

    latLong
    implemented signal in output to send the actual position to the parent object

--- src/app/modules/geolocation/components/track/track.component.ts
+++ src/app/modules/geolocation/components/track/track.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, ChangeDetectionStrategy } from '@angular/core';import { NodeStyleEventEmitter } from 'rxjs/internal/observable/fromEvent';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.scss'],
+changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class TrackComponent implements OnInit {
+trackingState = false
+trackChange(data){
+	console.log('tracking on',this.trackingState)
+}
 
   constructor() { }
+  @Output() latLong:EventEmitter<{lat:number,lng:number}> = new EventEmitter()
 
   track(){
 	  console.log('tracking')
@@ -20,6 +26,7 @@ export class TrackComponent implements OnInit {
 	  let success =(pos)=>{
 		  var crd = pos.coords;
 		  console.log("actual pos",pos)
+		  this.latLong.emit({lat:pos.coords.latitude,lng:pos.coords.longitude})
 		  alert(`current position ${pos.coords.latitude}, ${pos.coords.longitude}`)
 	  }
 	  let error = (error)=>{
