a461f4030725fd8f4b2bd0cde3e4dc61274c9963
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-13 19:05:16 +0200

    model for tracking
    implemente model to use for tracking

---
+++ src/app/modules/geolocation/models/tracking.ts
@@ -0,0 +1,30 @@
+import { stringify } from "querystring"
+import { isConstructorDeclaration } from "typescript"
+import { Serializers } from "../../helpers/serializers"
+
+export class trackPosition{
+
+    latitude:number
+    longitude:number
+    driverKey="" 
+    timestamp:number
+    constructor(v?:{}){
+        this.load(v)
+
+    }
+
+    serialize(){
+    const serializers= new Serializers
+    return {
+        latitude:serializers.serialize2PositiveNumber( this.latitude),
+        longitude:serializers.serialize2PositiveNumber( this.longitude),
+        timestamp:serializers.serialize2PositiveNumber( this.timestamp),
+        key:serializers.serialize2String(this.driverKey)
+    }
+    }
+    load(v: {}) {
+       if(v){
+           Object.assign(v)
+       }
+    }
+}
