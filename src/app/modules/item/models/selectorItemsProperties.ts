fdfb4caa51a1c44755a0103218094a276844a03a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-16 12:29:19 +0200

    refactoring selector-Question
    to be more general, we use ItemModelInterface instead of CategoryModel

--- src/app/modules/item/models/selectorItemsProperties.ts
+++ src/app/modules/item/models/selectorItemsProperties.ts
@@ -1,9 +1,8 @@
 import { QuestionProperties } from '../../dynamic-form/models/questionproperties';
-import { CategoryModel } from 'src/app/models/CategoryModel';
 import { ItemServiceInterface } from './ItemServiceInterface';
 import { ItemModelInterface } from './itemModelInterface';
 // tslint:disable:semicolon
-export interface SelectorProperties extends QuestionProperties<CategoryModel> {
+export interface SelectorProperties extends QuestionProperties<ItemModelInterface> {
     service: ItemServiceInterface;
     text: string;
     createPopup
