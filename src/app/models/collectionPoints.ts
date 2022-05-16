590e7cc9d01c7942c111753fe5d9b6c0b1664082
arpho <damicogiuseppe77@gmail.com>
2022-05-16 18:31:57 +0200

    bug fixing
    for a wrong name the address was not properly instantiated, more over serialize returned void

--- src/app/models/collectionPoints.ts
+++ src/app/models/collectionPoints.ts
@@ -15,90 +15,97 @@ import { Utente } from "./Utente";
 
 
 export class CollectionPoint implements ItemModelInterface {
-    title: string;
-    note?: string;
-    address= new Address()
-    key: string;
-    quickActions?: QuickAction[];
-    archived?: boolean;
-    service?: ItemServiceInterface;
+  title: string;
+  note?: string;
+  address: Address = new Address()
+  key: string;
+  quickActions?: QuickAction[];
+  archived?: boolean;
+  service?: ItemServiceInterface;
 
-    load(v) {
-        if(v){
-        Object.assign(this, v)
-        this.title= this.title||v['denominazione']}
-        
-        if (v && v['indirizzo']) {
-            this.address = new Address(v['address'])
-        }
-        return this
-    }
-    getTitle(): Value {
-        return new Value({ value: this.title, label: 'punto di raccolta' })
-    }
-    getCountingText() {
-        return{plural: 'punti di raccolta',singular:'punto di raccolta'}
-    }
-    getNote(): Value {
-        return new Value({ value: this.note, label: 'note' })
-    }
-    build?(item: {}) {
-        this.load(item)
-        return this
-    }
-    isArchived?(): boolean {
-        return this.archived
-    }
-    archiveItem?(b: boolean) {
-        this.archived = b
-    }
-    isArchivable?(): boolean {
-        return true
-    }
-    getValue2(): Value {
-        const serializers= new Serializers()
-        return new Value({ value:serializers.serialize2String (this.address.fetchAddress()), label: 'indirizzo' })
-    }
-    getValue3(): Value {
-        return new Value({label:'indirizzo',value:this.address.fetchAddress()})
-    }
-    getValue4(): Value {
-        return new Value({label:'key',value:this.key})
-    }
-    setKey?(key: string): CollectionPoint {
-        this.key = key
-        return this
-    }
-    getEditPopup(item?: ItemModelInterface, service?: ItemServiceInterface) {
-        throw new Error("Method not implemented.");
-    }
-    initialize(item: {}): ItemModelInterface {
-        this.load(item)
-        return this
-    }
-    getAggregate(): Value {
-        throw new Error("Method not implemented.");
-    }
-    aggregateAction?() {
-        throw new Error("Method not implemented.");
-    }
-    hasQuickActions?(): boolean {
-        return false
-    }
-    serialize() {
-        const serializers = new Serializers()
-        var out = { title: serializers.serialize2String(this.title), note: serializers.serialize2String(this.note), }
-        if (this.key) {
-            out = { ...out, ...{ key: this.key } }
-        }
-
-    }
-    getElement(): { element: string; genere: Genere; } {
-        return { element: 'punto di raccolta', genere: 'o' }
+  load(v) {
+    if (v) {
+      Object.assign(this, v)
+      this.title = this.title || v['denominazione']
     }
 
-    constructor(v?:{}){
-        this.load(v)
+    if (v && v['address']) {
+      this.address = new Address(v['address'])
     }
+    console.log("loaded cp",this)
+    return this
+  }
+  getTitle(): Value {
+    return new Value({ value: this.title, label: 'punto di raccolta' })
+  }
+  getCountingText() {
+    return { plural: 'punti di raccolta', singular: 'punto di raccolta' }
+  }
+  getNote(): Value {
+    return new Value({ value: this.note, label: 'note' })
+  }
+  build?(item: {}) {
+    this.load(item)
+    return this
+  }
+  isArchived?(): boolean {
+    return this.archived
+  }
+  archiveItem?(b: boolean) {
+    this.archived = b
+  }
+  isArchivable?(): boolean {
+    return true
+  }
+  getValue2(): Value {
+    const serializers = new Serializers()
+    return new Value({ value: serializers.serialize2String(this.address.fetchAddress()), label: 'indirizzo' })
+  }
+  getValue3(): Value {
+    return new Value({ label: 'indirizzo', value: this.address.fetchAddress() })
+  }
+  getValue4(): Value {
+    return new Value({ label: 'key', value: this.key })
+  }
+  setKey?(key: string): CollectionPoint {
+    this.key = key
+    return this
+  }
+  getEditPopup(item?: ItemModelInterface, service?: ItemServiceInterface) {
+    throw new Error("Method not implemented.");
+  }
+  initialize(item: {}): ItemModelInterface {
+    this.load(item)
+    return this
+  }
+  getAggregate(): Value {
+    throw new Error("Method not implemented.");
+  }
+  aggregateAction?() {
+    throw new Error("Method not implemented.");
+  }
+  hasQuickActions?(): boolean {
+    return false
+  }
+  serialize() {
+    const serializers = new Serializers()
+
+    const getMethods = (obj) => Object.getOwnPropertyNames(obj).filter(item => typeof obj[item] === 'function')
+    console.log('metodi:',getMethods(this.address))
+    console.log("address", this.address.serialize())
+    var out = { title: serializers.serialize2String(this.title), note: serializers.serialize2String(this.note), address: this.address.serialize() }
+    if (this.key) {
+      out = { ...out, ...{ key: this.key } }
+    }
+    return out
+
+  }
+  getElement(): { element: string; genere: Genere; } {
+    return { element: 'punto di raccolta', genere: 'o' }
+  }
+
+  constructor(v?: {}) {
+    this.load(v)
+  }
 
 }
