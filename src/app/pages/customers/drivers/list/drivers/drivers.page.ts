9dd65b33dafdfa3aaae6a5de824e7e49df8f9297
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-05-17 08:47:06 +0200

    driversPage ready

--- src/app/pages/customers/drivers/list/drivers/drivers.page.ts
+++ src/app/pages/customers/drivers/list/drivers/drivers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Driver } from 'src/app/models/Driver';
+import { UserType } from 'src/app/models/usersType';
 import { DriversService } from 'src/app/services/autisti/drivers.service';
 import { NewDriverPage } from '../../create/new-driver/new-driver.page';
 import { UpdateDriverPage } from '../../edit/update-driver/update-driver.page';
@@ -13,8 +15,12 @@ export class DriversPage implements OnInit {
   constructor(public service:DriversService) { }
   createModalPage = NewDriverPage
   editModalPage = UpdateDriverPage
+  filterFunction: (utente: Driver) => boolean;
+  ngOnInit(): void {
 
-  ngOnInit() {
+    this.filterFunction = (utente:Driver)=>{
+      return utente.userType == UserType.autista
+    }
   }
 
 }
