5c7da5395461ca63fe561be4e9dd701fa5b45c5f
arpho <damicogiuseppe77@gmail.com>
2022-05-21 22:45:56 +0200

    app.initialize
    corrected initialization and typo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,7 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {db} from "./configs/firebase";
 if (admin.apps.length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
-      projectId: functions.config().project.id,
-      clientEmail: functions.config().client.email,
-    }),
-    databaseURL: `https://${functions.config().project.id}.firebaseio.com`,
-  });
+  admin.initializeApp();
 }
 exports.addAddminRole = functions.https.onCall((data) => {
   return admin.auth().getUserByEmail(data.email).then((user) => {
@@ -43,9 +36,8 @@ exports.addCustomClaim = functions.https.onCall((data) => {
 
 exports.insertUser = functions.https.onCall((req)=>{
   const reference = "userProfile";
-
   db.ref(reference).push(req).then(()=>{
-    return {"message": `utente ${req.email} inseerito nel db`};
+    return {"message": `utente ${req.email} inserito nel db`};
   }).catch((error)=>{
     return error;
   });
