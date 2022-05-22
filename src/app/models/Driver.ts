5fe05dbee2aad96014b3e9304018e5de27d377eb
arpho <damicogiuseppe77@gmail.com>
2022-05-22 18:44:39 +0200

    vehicle setter and  getter
    implemented seter and getter for vwehicle

--- src/app/models/Driver.ts
+++ src/app/models/Driver.ts
@@ -1,10 +1,31 @@
+import { Serializers } from "../modules/helpers/serializers";
 import { UserType } from "./usersType";
 import { Utente } from "./Utente";
 import { Vehicle } from "./vehicle";
 
 export class Driver extends Utente{
     busKey:string
-    vehicle:Vehicle
+    vehicleKey: string
+    private _vehicle:Vehicle
+    set vehicle(vehicle:Vehicle){
+      this.vehicleKey = vehicle.key
+
+    }
+
+    get vehicle(){
+      return this._vehicle
+    }
+
+   
+    serialize(): { level: number; telephones: { numero: string; note: string; }[]; archived: boolean; dor: string; userType: UserType; uid: string; birthDate: string | { year: number; month: number; day: number; }; email: string; firstName: string; lastName: string; enabled: boolean; 
+  } {
+      const serializers= new Serializers
+      return {
+        ...super.serialize(),
+        ...{vehicleKey:serializers.serialize2String( this.vehicle.key)}
+      }
+    }
+    
     constructor(v?:{}){
         super(v)
         this.userType= UserType.autista
