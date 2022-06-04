5bf4663b87f66c79ba799920f0e2b532bb8b17b8
arpho <damicogiuseppe77@gmail.com>
2022-06-04 10:08:23 +0200

    myDateComponent
    implemented template and ControlValueAccessor

--- src/app/modules/dynamic-form/components/my-date-time/my-date-time.component.ts
+++ src/app/modules/dynamic-form/components/my-date-time/my-date-time.component.ts
@@ -1,14 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { DateModel } from 'src/app/modules/user/models/birthDateModel';
 
 @Component({
   selector: 'app-my-date-time',
   templateUrl: './my-date-time.component.html',
   styleUrls: ['./my-date-time.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: MyDateTimeComponent
+    }
+  ]
+
+
+
+
+
+  
 })
-export class MyDateTimeComponent implements OnInit {
+export class MyDateTimeComponent implements OnInit,ControlValueAccessor { 
+  private onChange: Function = (password: string) => { };
+  disabled: boolean
+// tslint:disable-next-line: ban-types
+private onTouch: Function = () => { };
+  @Input() myDate:DateModel
+  @Input() presentation:string
 
-  constructor() { }
+  constructor(public modal:ModalController) { }
+  writeValue(obj: DateModel): void {
+   this.myDate = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(!this.myDate){
+      this.myDate= new DateModel(new Date())
+    console.log("today",this.myDate.formatDate(),typeof this.myDate)
+    }
+  }
+
+showDate(){
+  console.log("showing date",this.myDate)
+  return this.myDate?this.myDate.formatDate():new DateModel(new Date()).italianFormatDate()
+}
 
 }
