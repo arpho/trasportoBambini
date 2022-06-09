93841d56196f281da247d0ca405e067c045cb3e9
arpho <damicogiuseppe77@gmail.com>
2022-06-09 14:14:00 +0200

    completing BusRide

--- src/app/models/busRide.ts
+++ src/app/models/busRide.ts
@@ -7,59 +7,63 @@ import { Driver } from "./Driver";
 import { RideStatus } from "./RideStatus";
 import { StudentPresence } from "./studentStatus";
 import { Vehicle } from "./vehicle";
-import {LatLong} from "../modules/geolocation/models/latlong"
+import { LatLong } from "../modules/geolocation/models/latlong"
+import { Serializers } from "../modules/helpers/serializers";
 
 
-export class BusRide implements ItemModelInterface{
+export class BusRide implements ItemModelInterface {
   note?: string;
   key: string;
-  startTime:DateModel
+  startTime: DateModel
   endTime: DateModel
-  _driver:Driver = new Driver()
-  _driverKey:string
-  busKey:string
-  tracking:LatLong[]
-  _bus:Vehicle = new Vehicle()
-  set bus(bus:Vehicle){
-    this._bus = bus
-    this.busKey= bus.key
-  }
-  students:StudentPresence[]
-  status:RideStatus
-  
+  _driver: Driver = new Driver()
+  driverKey: string
+  busKey: string
+  tracking: LatLong[]
+  _bus: Vehicle = new Vehicle()
 
-  get bus(){
+  set bus(bus: Vehicle) {
+    this._bus = bus
+    this.busKey = bus.key
+  }
+  students: StudentPresence[]
+  status: RideStatus
+
+
+  get bus() {
     return this._bus
   }
 
-  set driver(driver:Driver){
-    this._driver= driver
-    this._driverKey= driver.key
+  set driver(driver: Driver) {
+    this._driver = driver
+    this.driverKey = driver.key
   }
 
-  get driver(){
+  get driver() {
     return this._driver
   }
 
-  get title(){
+  get title() {
     return `tragitto del ${this.startTime.formatFullDate()}`
   }
   quickActions?: QuickAction[];
-  archived?: boolean= false
+  archived?: boolean = false
   service?: ItemServiceInterface;
   getTitle(): Value {
-    return new Value({value:this.title,label:"denominaione"})
+    return new Value({ value: this.title, label: "denominaione" })
   }
   getCountingText(): { singular: string; plural: string; } {
-    return {singular:"corsa",plural:"corse"};
+    return { singular: "corsa", plural: "corse" };
   }
   getNote(): Value {
-    return  new Value({value:this.note,label:"note"})
+    return new Value({ value: this.note, label: "note" })
   }
-  load?(data,next?: () => void) {
-    Object.assign(this,data)
+  load?(data, next?: () => void) {
+    Object.assign(this, data)
+    this.startTime = new DateModel(new Date(this.startTime))
+    this.endTime = new DateModel(new Date(this.endTime))
     return this
-    
+
   }
   isArchived?(): boolean {
     return this.archived
@@ -71,18 +75,18 @@ export class BusRide implements ItemModelInterface{
     return true
   }
   getValue2(): Value {
-    return new Value({value:this.status,label:"destinazione"})
+    return new Value({ value: this.status, label: "destinazione" })
   }
   getValue3(): Value {
-    return new Value( {
+    return new Value({
       value: this.bus.getTitle().value,
-      label:"bus"
+      label: "bus"
     })
   }
   getValue4(): Value {
     return new Value({
-      value:this._driver? this.driver.getTitle().value:"",
-      label:"autista"
+      value: this._driver ? this.driver.getTitle().value : "",
+      label: "autista"
     })
   }
   setKey?(key: string): ItemModelInterface {
@@ -105,14 +109,28 @@ export class BusRide implements ItemModelInterface{
     throw new Error("Method not implemented.");
   }
   serialize() {
-    return 
+    const serializers = new Serializers()
+    return {
+      key: serializers.serialize2String(this.key),
+      archived: this.archived,
+      note: serializers.serialize2String(this.note),
+      startTime: serializers.serialize2String(this.startTime.formatFullDate()),
+      endTime: serializers.serialize2String(this.endTime.formatFullDate()),
+      students: serializers.serialize2Array(this.students),
+      driverKey: serializers.serialize2String(this.driverKey),
+      busKey: serializers.serialize2String(this.busKey),
+      status: serializers.serialize2String(RideStatus[this.status])
+
+    }
   }
   getElement(): { element: string; genere: Genere; } {
-    return {element:"corsa",genere:'a'}
+    return { element: "corsa", genere: 'a' }
   }
 
-  constructor(v?:{}){
+  constructor(v?: {}) {
     this.load(v)
   }
+
+
 
 }
