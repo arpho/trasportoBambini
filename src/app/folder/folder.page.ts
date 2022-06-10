d5d2bf7ad2bb579c273aa915ab7843888d59be2c
arpho <damicogiuseppe77@gmail.com>
2022-06-10 23:48:28 +0200

    spaghetti code
    code messed up waiting to fix bug on addCustom claims

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -5,15 +5,18 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Observable } from 'rxjs';
 import { UserModel } from '../modules/user/models/userModel';
 import { UsersService } from '../modules/user/services/users.service';
+import { Utente } from '../models/Utente';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
+
 export class FolderPage implements OnInit {
   public folder: string;
   log = console.log.bind(document)
   latLon:{lat:number,lng:number}
+loggedUser:Utente
   constructor(private activatedRoute: ActivatedRoute,
     private router:Router,
     public User:UsersService,
@@ -47,9 +50,18 @@ track(){
     const auth = getAuth()
 	
  onAuthStateChanged(auth,async (user)=>{
+  
    if(user) {
 	   const token = await user.getIdTokenResult(true)
      this.log('user ok è',user)
+     const customer  = this.User.getItemByEmail(user.email,(user)=>{
+       if(user){
+      this.loggedUser = user
+      console.log("user by email",user)}
+     })
+     console.log("user by email",this.loggedUser)
+
+     this.User.getItem
 	 console.log('token.claims',token.claims)
    }
    else{
