617bcbca847142ab6bce7be36c444cc467b18641
arpho <damicogiuseppe77@gmail.com>
2022-06-11 20:14:18 +0200

    dismissing update collectionPoint
    implemented relative code

--- src/app/pages/collectionPoints/edit/update-collection-point/update-collection-point.page.ts
+++ src/app/pages/collectionPoints/edit/update-collection-point/update-collection-point.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
+import { ModalController, NavParams } from '@ionic/angular';
 import { CollectionPoint } from 'src/app/models/collectionPoints';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { TextAreaBox } from 'src/app/modules/dynamic-form/models/question-textArea';
@@ -21,7 +21,8 @@ export class UpdateCollectionPointPage implements OnInit {
 
   constructor(
     public navParams: NavParams,
-    public service: CollectionPointsService
+    public service: CollectionPointsService,
+    public modalCtrl:ModalController
   ) { }
 
   ngOnInit() {
@@ -37,6 +38,10 @@ export class UpdateCollectionPointPage implements OnInit {
   filter(ev) {
     console.log("typing", ev)
   }
+
+  dismiss(point?) {
+		this.modalCtrl.dismiss(point)
+	} 
 
   submit(ev) {
     this.collectionPoint.load(ev)
