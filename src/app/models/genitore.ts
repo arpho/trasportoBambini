c0cd9b8913fa98708a2043ff205050c2b7006c7e
arpho <damicogiuseppe77@gmail.com>
2022-06-12 13:55:06 +0200

    refactoring parentsModel
    better keys names for seriialize  and populate children

--- src/app/models/genitore.ts
+++ src/app/models/genitore.ts
@@ -1,51 +1,62 @@
 
 import { Address } from "../modules/geolocation/models/Address";
 import { Serializers } from "../modules/helpers/serializers";
+import { Studente } from "./studente";
 import { UserType } from "./usersType";
 import { Utente } from "./Utente";
 
 export class Genitore extends Utente {
-    children: Array<string>
-    userType = UserType.genitore
-    address: Address = new Address()
+  childrenKeys: Array<string>
+  _children: Array<Studente>
+  userType = UserType.genitore
+  address: Address = new Address()
+  set children(children: Array<Studente>) {
+    this._children = children
+    this.childrenKeys = this._children.map(child => {
+      return child.key
+    })
+  }
+  get children() {
+    return this._children
+  }
 
 
-    load(v: {}) {
-        Object.assign(this, v)
+  load(v: {}) {
+    Object.assign(this, v)
 
-        if (v && v['address']) {
+    if (v && v['address']) {
 
-            this.address = new Address({
-                'street': v['address']['street'],
-                'cap': v['address']['cap'],
-                'longitude': v['address']['longitude'],
-                'latitude': v['address']['latitude'],
-                'number': v['address']['number'],
-                'province': v['address']['province'],
-                'city': v['address']['city']
-            })
-        }
-
-        return this
+      this.address = new Address({
+        'street': v['address']['street'],
+        'cap': v['address']['cap'],
+        'longitude': v['address']['longitude'],
+        'latitude': v['address']['latitude'],
+        'number': v['address']['number'],
+        'province': v['address']['province'],
+        'city': v['address']['city']
+      })
     }
 
-    serialize() {
-        const serializers = new Serializers()
-        return { ...super.serialize(), ...{ figli: serializers.serialize2Array(this.children), address: this.address.serialize() } }
-    }
+    return this
+  }
 
-    getElement(): { element: string; genere: "o"; } {
-        return { element: 'genitore', genere: 'o' }
-    }
-    getCountingText(): { plural: string; singular: string; } {
-        return { plural: 'genitori', singular: 'genitore' }
-    }
+  serialize() {
+    const serializers = new Serializers()
+    return { ...super.serialize(), ...{ childrenKeys: serializers.serialize2Array(this.childrenKeys), address: this.address.serialize() } }
+  }
 
-    constructor(v?: {}) {
+  getElement(): { element: string; genere: "o"; } {
+    return { element: 'genitore', genere: 'o' }
+  }
+  getCountingText(): { plural: string; singular: string; } {
+    return { plural: 'genitori', singular: 'genitore' }
+  }
 
-        super(v)
-        this.load(v)
-        this.userType = UserType.genitore
-    }
+  constructor(v?: {}) {
+
+    super(v)
+    this.load(v)
+    this.userType = UserType.genitore
+  }
 
 }
