3e9725248aeabbc955d3e5b88908a6adacd110e1
arpho <damicogiuseppe77@gmail.com>
2022-06-12 09:47:02 +0200

    controlValueAccessor
    defgined component as control Value accessor

--- src/app/modules/dynamic-form/components/list-selector/list-selector.component.ts
+++ src/app/modules/dynamic-form/components/list-selector/list-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 import { ItemServiceInterface } from 'src/app/modules/item/models/ItemServiceInterface';
 import { NewStudentPage } from 'src/app/pages/customers/students/create/new-student/new-student.page';
@@ -8,8 +9,17 @@ import { SelectorQuestion } from '../../models/question-selector';
   selector: 'app-list-selector',
   templateUrl: './list-selector.component.html',
   styleUrls: ['./list-selector.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: ListSelectorComponent
+    },
+ 
+  ]
 })
-export class ListSelectorComponent implements OnInit {
+export class ListSelectorComponent implements OnInit,ControlValueAccessor {
+  itemsList: ItemModelInterface[]=[]
   @Input() text: string
   // tslint:disable: no-input-rename
   // tslint:disable-next-line: variable-name
@@ -21,14 +31,30 @@ export class ListSelectorComponent implements OnInit {
   @Input() filterFunction: (item: ItemModelInterface) => boolean
   @Input() sorterFunction: (a: ItemModelInterface, b: ItemModelInterface) => number
   hideSubmitButton = true
-
- 
+  private onTouch: Function = () => { };
+  private onValidationChange: any = () => { };
+  private onChange: Function = (password: string) => { };
   formFields:any[] 
+  disabled:boolean= false
+  touched = false
+
 
   constructor() { }
+  writeValue(obj: any): void {
+    this.itemsList = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled= isDisabled
+  }
 
   ngOnInit() {
-    
+
     this.formFields = [new SelectorQuestion({
       service:this.service,
       text:this.text,
@@ -36,6 +62,13 @@ export class ListSelectorComponent implements OnInit {
       label:"",
       key:"selector"
     })]
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouch();
+      this.touched = true;
+    }
   }
 
 }
