c146caf06f08772e6cee24b2da91d5684974883f
arpho <damicogiuseppe77@gmail.com>
2022-06-12 19:50:52 +0200

    Parent to parent's view

--- src/app/components/parents-view/parents-view.component.ts
+++ src/app/components/parents-view/parents-view.component.ts
@@ -1,16 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { Genitore } from 'src/app/models/genitore';
+import { Studente } from 'src/app/models/studente';
 
 @Component({
   selector: 'app-parents-view',
   templateUrl: './parents-view.component.html',
   styleUrls: ['./parents-view.component.scss'],
 })
-export class ParentsViewComponent implements OnInit {
+export class ParentsViewComponent implements OnInit,OnChanges {
   @Input() parent:Genitore
+_studentsList:BehaviorSubject<Array<Studente>> = new BehaviorSubject([])
+readonly students:Observable<Array<Studente>> = this._studentsList.asObservable()
+
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+   console.log("cyhanges",changes)
+    if(this.parent){
+      console.log("studenti",this.parent.children)
+    this._studentsList.next(this.parent.children)}
+   
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+ 
+  }
 
 }
