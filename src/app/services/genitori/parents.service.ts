438ecae107994fd5d05dae6a6dabbff2d72c82dc
arpho <damicogiuseppe77@gmail.com>
2022-06-12 14:00:39 +0200

    removed unused import

--- src/app/services/genitori/parents.service.ts
+++ src/app/services/genitori/parents.service.ts
@@ -3,7 +3,6 @@ import { Genitore } from 'src/app/models/genitore';
 import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 import { CustomersFactoryService } from '../customers/business/customers-constructor.service';
 import { CustomersService } from '../customers/customers.service';
-import { VehiclesService } from '../vehicles/vehicles.service';
 
 @Injectable({
   providedIn: 'root'
