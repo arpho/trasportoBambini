5977f1e6b550de0025f3a93d50b3282d12313fd0
arpho <damicogiuseppe77@gmail.com>
2022-06-23 15:40:09 +0200

    implementing
    code to handle the form

--- src/app/pages/customers/clerks/create/new-clerk/new-clerk.page.ts
+++ src/app/pages/customers/clerks/create/new-clerk/new-clerk.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Addetto } from 'src/app/models/Addetto';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { EmailQuestion } from 'src/app/modules/dynamic-form/models/question-email';
+import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 
 @Component({
   selector: 'app-new-clerk',
@@ -8,6 +11,32 @@ import { Addetto } from 'src/app/models/Addetto';
   styleUrls: ['./new-clerk.page.scss'],
 })
 export class NewClerkPage implements OnInit {
+
+
+  clerk = new Addetto()
+
+  formFields: any[] =
+    [
+      new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.clerk.firstName }),
+      new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.clerk.lastName }),
+      new EmailQuestion({
+        key: "email",
+        label: "email",
+        value: this.clerk.email,
+        required: true
+      }),
+      new AddressQuestion({ key: 'address', label: 'indirizzo', value: this.clerk.address })
+    ]
+  filter(ev) {
+    console.log("typing", ev)
+  }
+
+  submit(ev) {
+    this.clerk.load(ev)
+    console.log("submitting", ev)
+    console.log("clerk", this.clerk)
+
+  }
 
   constructor(public modalCtrl:ModalController) { }
   dismiss(clerk?:Addetto){
