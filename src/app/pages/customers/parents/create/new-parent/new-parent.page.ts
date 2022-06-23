8dd2531da9fc8a0be53991cf9e4303a90244c8fd
arpho <damicogiuseppe77@gmail.com>
2022-06-23 20:21:11 +0200

    listSelectorQuestion
    used listSelectorQuestion in new parent's form

--- src/app/pages/customers/parents/create/new-parent/new-parent.page.ts
+++ src/app/pages/customers/parents/create/new-parent/new-parent.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Genitore } from 'src/app/models/genitore';
+import { UserType } from 'src/app/models/usersType';
+import { Utente } from 'src/app/models/Utente';
 import { AddressQuestion } from 'src/app/modules/dynamic-form/models/question-address';
 import { EmailQuestion } from 'src/app/modules/dynamic-form/models/question-email';
+import { ListSelectorQuestion } from 'src/app/modules/dynamic-form/models/question-selector-list';
 import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 import { CustomersService } from 'src/app/services/customers/customers.service';
+import { NewStudentPage } from '../../../students/create/new-student/new-student.page';
 
 @Component({
   selector: 'app-new-parent',
@@ -13,18 +17,9 @@ import { CustomersService } from 'src/app/services/customers/customers.service';
   styleUrls: ['./new-parent.page.scss'],
 })
 export class NewParentPage implements OnInit {
+ 
   parent = new Genitore()
-  public formFields = [
-    new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.parent.firstName }),
-    new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.parent.lastName }),
-    new EmailQuestion({
-      key:"email",
-      label:"email",
-      value:this.parent.email,
-      required:true
-    }),
-    new AddressQuestion({ key: 'indirizzo', label: 'indirizzo', value: this.parent.address })
-  ]
+  public formFields = [ ]
 
 
   filter(ev) {
@@ -65,6 +60,31 @@ export class NewParentPage implements OnInit {
   }
 
   ngOnInit() {
+     const filterStudent = (item:Utente)=>{
+    return item.userType==UserType.studente
+  }
+
+  this.formFields = [
+    new TextboxQuestion({ key: 'firstName', label: 'nome', value: this.parent.firstName }),
+    new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.parent.lastName }),
+    new EmailQuestion({
+      key:"email",
+      label:"email",
+      value:this.parent.email,
+      required:true
+    }),
+    new AddressQuestion({ key: 'indirizzo', label: 'indirizzo', value: this.parent.address }),
+    new ListSelectorQuestion({
+      service:this.service,
+      text:"studenti",
+      value:this.parent.children,
+      filterShownItems:filterStudent,
+      hideSelectedItem:true,
+      createPopup:NewStudentPage,
+      label:"figli",
+      key:"children"
+    })
+  ]
   }
 
 }
