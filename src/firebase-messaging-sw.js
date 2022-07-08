1a61694b05919b48bc15ed7c4b9d72c0f3fc45fb
arpho <damicogiuseppe77@gmail.com>
2022-07-08 11:47:58 +0200

    sevice worker fo fcm
    implemented the service worker following  the instruction in firebase documentation

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+const { FirebaseError } = require("firebase/app")
+import * as firebase from firebase
+import {configs} from "./app/configs/credentials"
+importScripts('https://www.gstatic.com/firebasewjs/3.5.2/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasewjs/3.5.2/firebase-messaging.js')
+firebase.initializeApp(configs.firebase)
+const messaging = firebase.messaging()
+messaging.setBackgroundMessageHandler(payload=>{
+  console.log("background message",payload);
+
+  const promiseChain = clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+    })
+    .then((windowClients) => {
+    for (let i = 0; i < windowClients.length; i++) {
+      const windowClient = windowClients[i];
+      windowClient.postMessage(data);
+    }
+    })
+    .then(() => {
+    return registration.showNotification('my notification title');
+    });
+    return promiseChain;
+  return self.registration.showNotification("Title", {body: 'New notification.',payload})
+});
+/* if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  } */
